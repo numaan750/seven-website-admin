9656c72497e31097e555ebd7adb27c71e5cb5c41
Numaan Ali <alinumaan35@gmail.com>
2025-10-22 00:28:57 -0700

    aaaa

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Use a custom build folder (avoids OneDrive readlink errors)
-  distDir: 'build',
-
-  // Tell Next.js the real project root (fixes "multiple lockfiles" warning)
-  outputFileTracingRoot: "C:\\Users\\Numaan ali\\OneDrive\\Desktop\\7app-admin",
-
   // Optional: disable symlinks to avoid readlink errors on OneDrive
   webpack: (config) => {
     config.resolve.symlinks = false;
